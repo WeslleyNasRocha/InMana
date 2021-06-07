4a69085a2a4c2214a9b87b7c3bf056c582643955
Weslley Rocha <weslleytato.12@gmail.com>
2021-06-07 12:19:34 -0300

    aula 4: scheduler

--- lib/inmana/supply/expiration_notification.ex
+++ lib/inmana/supply/expiration_notification.ex
@@ -3,13 +3,14 @@ defmodule Inmana.Supply.ExpirationNotification do
   alias Inmana.Supply.{ExpirationEmail, GetByExpiration}
 
   def send do
-    data = GetByExpiration.call()
+    GetByExpiration.call()
+    |> Task.async_stream(fn {to_email, supplies} -> send_email(to_email, supplies) end)
+    |> Stream.run()
+  end
 
-    data
-    |> Enum.each(fn {to_email, supplies} ->
-      to_email
-      |> ExpirationEmail.create(supplies)
-      |> Mailer.deliver_later!()
-    end)
+  defp send_email(to_email, supplies) do
+    to_email
+    |> ExpirationEmail.create(supplies)
+    |> Mailer.deliver_later!()
   end
 end
